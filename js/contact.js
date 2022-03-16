01f8bfb558afec3a40e3f898bb0fc8a9521edb23
hreinn <hreinngy@gmail.com>
2022-03-16 11:29:05 +0100

    Working on adding a contact form

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("form");
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
