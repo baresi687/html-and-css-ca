ea236bc81be6c2341637cc27aab9c58506fff6c5
hreinn <hreinngy@gmail.com>
2022-03-18 21:18:59 +0100

    Improved and styled the contact form. Cleaning up html

--- js/contact.js
+++ js/contact.js
@@ -4,9 +4,8 @@ const lastName = document.querySelector("#last-name");
 const email = document.querySelector("#email");
 const message = document.querySelector("#message");
 
-
-firstName.addEventListener("focus", function (event)  {
-  firstName.onkeyup = (event) => {
+firstName.addEventListener("focus", function ()  {
+  firstName.onkeyup = () => {
     if (!validateInput(firstName.value, 3)) {
       document.querySelector("#first-name ~ i").classList.remove("checkmark-show")
     } else {
@@ -14,19 +13,21 @@ firstName.addEventListener("focus", function (event)  {
       document.querySelector("#first-name ~ .input-error").innerHTML = ""
     }
   }
-  firstName.onblur = (event) => {
-   if (!validateInput(firstName.value, 3)) {
-     firstName.classList.add("input-error-onblur")
-     document.querySelector("#first-name ~ .input-error").innerHTML = "First name must be 3 characters or more"
-   } else {
-     firstName.classList.remove("input-error-onblur")
-     document.querySelector("#first-name ~ .input-error").innerHTML = ""
+  firstName.onblur = () => {
+   if (firstName.value) {
+     if (!validateInput(firstName.value, 3)) {
+       firstName.classList.add("input-error-onblur")
+       document.querySelector("#first-name ~ .input-error").innerHTML = "First name must be 3 characters or more"
+     } else {
+       firstName.classList.remove("input-error-onblur")
+       document.querySelector("#first-name ~ .input-error").innerHTML = ""
+     }
    }
   }
 })
 
-lastName.addEventListener("focus", function (event)  {
-  lastName.onkeyup = (event) => {
+lastName.addEventListener("focus", function ()  {
+  lastName.onkeyup = () => {
     if (!validateInput(lastName.value, 3)) {
       document.querySelector("#last-name ~ i").classList.remove("checkmark-show")
     } else {
@@ -34,19 +35,21 @@ lastName.addEventListener("focus", function (event)  {
       document.querySelector("#last-name ~ .input-error").innerHTML = ""
     }
   }
-  lastName.onblur = (event) => {
-    if (!validateInput(lastName.value, 3)) {
-      lastName.classList.add("input-error-onblur")
-      document.querySelector("#last-name ~ .input-error").innerHTML = "Last name must be 3 characters or more"
-    } else {
-      lastName.classList.remove("input-error-onblur")
-      document.querySelector("#last-name ~ .input-error").innerHTML = ""
+  lastName.onblur = () => {
+    if (lastName.value) {
+      if (!validateInput(lastName.value, 3)) {
+        lastName.classList.add("input-error-onblur")
+        document.querySelector("#last-name ~ .input-error").innerHTML = "Last name must be 3 characters or more"
+      } else {
+        lastName.classList.remove("input-error-onblur")
+        document.querySelector("#last-name ~ .input-error").innerHTML = ""
+      }
     }
   }
 })
 
-email.addEventListener("focus", function (event)  {
-  email.onkeyup = (event) => {
+email.addEventListener("focus", function ()  {
+  email.onkeyup = () => {
     if (!validateEmail(email.value)) {
       document.querySelector("#email ~ i").classList.remove("checkmark-show")
     } else {
@@ -54,19 +57,21 @@ email.addEventListener("focus", function (event)  {
       document.querySelector("#email ~ .input-error").innerHTML = ""
     }
   }
-  email.onblur = (event) => {
-    if (!validateEmail(email.value)) {
-      email.classList.add("input-error-onblur")
-      document.querySelector("#email ~ .input-error").innerHTML = "Email format is not valid"
-    } else {
-      email.classList.remove("input-error-onblur")
-      document.querySelector("#email ~ .input-error").innerHTML = ""
+  email.onblur = () => {
+    if (email.value) {
+      if (!validateEmail(email.value)) {
+        email.classList.add("input-error-onblur")
+        document.querySelector("#email ~ .input-error").innerHTML = "Email format is not valid"
+      } else {
+        email.classList.remove("input-error-onblur")
+        document.querySelector("#email ~ .input-error").innerHTML = ""
+      }
     }
   }
 })
 
-message.addEventListener("focus", function (event)  {
-  message.onkeyup = (event) => {
+message.addEventListener("focus", function ()  {
+  message.onkeyup = () => {
     if (!validateInput(message.value, 25)) {
       document.querySelector("#message ~ i").classList.remove("checkmark-show")
     } else {
@@ -74,13 +79,15 @@ message.addEventListener("focus", function (event)  {
       document.querySelector("#message ~ .input-error").innerHTML = ""
     }
   }
-  message.onblur = (event) => {
-    if (!validateInput(message.value, 25)) {
-      message.classList.add("input-error-onblur")
-      document.querySelector("#message ~ .input-error").innerHTML = "Message must be 25 characters or more"
-    } else {
-      message.classList.remove("input-error-onblur")
-      document.querySelector("#message ~ .input-error").innerHTML = ""
+  message.onblur = () => {
+    if (message.value) {
+      if (!validateInput(message.value, 25)) {
+        message.classList.add("input-error-onblur")
+        document.querySelector("#message ~ .input-error").innerHTML = "Message must be 25 characters or more"
+      } else {
+        message.classList.remove("input-error-onblur")
+        document.querySelector("#message ~ .input-error").innerHTML = ""
+      }
     }
   }
 })
@@ -103,13 +110,10 @@ form.addEventListener("submit", (event) => {
     message.classList.add("input-error-onblur")
     document.querySelector("#message ~ .input-error").innerHTML = "Message must be 25 characters or more"
   }
-
   if (validateInput(firstName.value, 3) && validateInput(lastName.value, 3) && validateEmail(email.value) && validateInput(message.value, 25)) {
-    /*document.querySelectorAll("form div i").classList.remove("checkmark-show")*/
     document.querySelector(".form-success").classList.add("show")
     form.reset();
   }
-
 })
 
 function validateInput(value, len) {
